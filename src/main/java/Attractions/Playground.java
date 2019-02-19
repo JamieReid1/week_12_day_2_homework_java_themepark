e208a9c68136bdd750217417693082a88be02d3d
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-19 17:30:35 +0000

    Playground implements ISecurity, passing tests.

--- src/main/java/Attractions/Playground.java
+++ src/main/java/Attractions/Playground.java
@@ -1,8 +1,22 @@
 package Attractions;
 
-public class Playground extends Attraction {
+import Interfaces.ISecurity;
+import Visitors.Visitor;
+
+public class Playground extends Attraction implements ISecurity {
 
     public Playground(String name) {
         super(name);
     }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        return visitor.getAge() <= 15;
+//        if (visitor.getAge() <= 15){
+//            return true;
+//        } else {
+//            return false;
+//        }
+    }
+
 }
--- src/test/java/AttractionsTests/PlaygroundTest.java
+++ src/test/java/AttractionsTests/PlaygroundTest.java
@@ -1,6 +1,7 @@
 package AttractionsTests;
 
 import Attractions.Playground;
+import Visitors.Visitor;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,11 +9,28 @@ import static org.junit.Assert.assertEquals;
 public class PlaygroundTest {
 
     Playground swingsAndThings;
+    Visitor child;
+    Visitor adult;
+
 
     @Test
     public void hasName(){
         swingsAndThings = new Playground("Swings & Things");
         assertEquals("Swings & Things", swingsAndThings.getName());
+    }
+
+    @Test
+    public void isAllowed(){
+        swingsAndThings = new Playground("Swings & Things");
+        child = new Visitor(8, 1.1, 10);
+        assertEquals(true, swingsAndThings.isAllowedTo(child));
+    }
+
+    @Test
+    public void isNotAllowed(){
+        swingsAndThings = new Playground("Swings & Things");
+        adult = new Visitor(30, 18, 100);
+        assertEquals(false, swingsAndThings.isAllowedTo(adult));
     }
 
 }
