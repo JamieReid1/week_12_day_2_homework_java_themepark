ef510532aa7ca83038b6aac613c52643b119b1e3
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-19 16:52:08 +0000

    Creates CandyFlossStall, extends Stall, passing tests.

---
+++ src/test/java/StallsTests/CandyFlossStallTest.java
@@ -0,0 +1,35 @@
+package StallsTests;
+
+import Stalls.CandyFlossStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CandyFlossStallTest {
+
+    CandyFlossStall funSpunCandy;
+
+    @Before
+    public void before(){
+
+        funSpunCandy = new CandyFlossStall("Fun Spun Candy", "Marge Simpson", 2);
+
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Fun Spun Candy", funSpunCandy.getName());
+    }
+
+    @Test
+    public void hasOwner(){
+        assertEquals("Marge Simpson", funSpunCandy.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot(){
+        assertEquals(2, funSpunCandy.getParkingSpot());
+    }
+    
+}
