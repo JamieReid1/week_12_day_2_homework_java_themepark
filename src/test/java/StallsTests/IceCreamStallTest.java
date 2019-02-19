160df992ae2e33f54f2a20a5c84f1be63c32b88a
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-19 16:44:59 +0000

    Creates IceCreamStall, extends Stall, passing tests.

--- src/test/java/StallsTests/IceCreamStallTest.java
+++ src/test/java/StallsTests/IceCreamStallTest.java
@@ -1,4 +1,35 @@
 package StallsTests;
 
+import Stalls.IceCreamStall;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IceCreamStallTest {
+
+    IceCreamStall flakesNShakes;
+
+    @Before
+    public void before(){
+
+        flakesNShakes = new IceCreamStall("Flakes N Shakes", "Beldar Conehead", 1);
+
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Flakes N Shakes", flakesNShakes.getName());
+    }
+
+    @Test
+    public void hasOwner(){
+        assertEquals("Beldar Conehead", flakesNShakes.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot(){
+        assertEquals(1, flakesNShakes.getParkingSpot());
+    }
+
 }
